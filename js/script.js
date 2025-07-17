6a22e20433439c6e49eb3f633684126416859f66
mikmak29 <mjogamaapple@gmail.com>
2025-07-17 09:43:54 +0800

    Responsive navbar Initial update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuButton = () => {
+    const drop_list = document.getElementById("drop_list")
+
+    if (drop_list) {
+        drop_list.classList.toggle("drop-list");
+    }
+
+    console.log("Hello")
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/*"
+    "./src/*",
+    "./js/*"
   ],
   theme: {
     extend: {
       screens: {
-        xs: "320px"
+        xs: "320px",
+        sm: ""
       },
       fontFamily: {
         sansation: ['Sansation', 'sans-serif'],
@@ -16,6 +18,17 @@ module.exports = {
       container: {
         center: true,
       },
+      colors: {
+        color_primary: '#121212',
+        color_secondary: '#efefef',
+        color_accent: '#CB7428',
+        color_gray: '#939DA5',
+        color_slate_light_blue: '#7C8EA4',
+        color_slate_dark_blue: '#939DA5',
+        color_slate_orange: '#8A7970',
+        color_bg_dark: '#1E2A2F',
+        color_bg_light: '#DFDDDC',
+      }
     },
   },
   plugins: [],

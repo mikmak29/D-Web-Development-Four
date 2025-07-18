8fac38c3d3d4adbdbe870f33e46bfbda4c355428
mikmak29 <mjogamaapple@gmail.com>
2025-07-18 16:26:29 +0800

    section-4 added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/*",
-    "./js/*"
+    "./js/*",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
